326a963318bab28bedefcd736f83d1888d05ef58
unknown <sridharswaminath@SEA0296L.seargindomain.local>
2025-02-22 20:25:47 +0800

    updated

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,13 +1,20 @@
-import { useEffect } from 'react/ts5.0'
-import { useState } from 'react/ts5.0'
+import { useEffect, useState } from 'react'
+
+const URL = 'https://api.spoonacular.com/recipes/complexSearch';
+const API_KEY = '836b394897894a459989f8487ca5009d';
 
 export default function Search() {
-  const [query, setQuery] = useState('')
-  const URL = 'https://api.spoonacular.com/recipes/complexSearch'
+  const [query, setQuery] = useState('pizza')
 
   // Syntax of the useEffect hook
   useEffect(() => {
-    function fetchFood() {}
+    async function fetchFood() {
+      const res = await fetch(`${URL}?query=${query}&apiKey=${API_KEY}`);
+      const data = await res.json();
+      console.log(data);
+      
+    }
+    fetchFood()
   }, [query])
   return (
     <div>
